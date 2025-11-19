e328389443fb2c302e2a34ea93246aa24ece2033
chivoi <a.lastovirya@gmail.com>
2025-11-19 14:54:16 -0600

    ignore proken anchors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,7 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
+  onBrokenAnchors: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
