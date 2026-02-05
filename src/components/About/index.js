884894eb26afe093b4162e1e8aaea423667e212c
chivoi <a.lastovirya@gmail.com>
2026-02-05 15:27:43 -0600

    edit sub-header and about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,16 +12,17 @@ function AboutCard() {
   return (
     <div className="container">
       <div className="text--left padding-horiz--md">
-        <h3 className='text--center'>Hi friend 👋 </h3>
-          <p>I'm Ana, a tech writer and developer based between the US and Australia. For the past 4.5 years I worked as a Software Engineer at Zendesk, where I built API integrations and iPaaS connectors, as well as wrote thorough docs and guides for what me and my team built. Apart from the recent experience in technical roles, I have a background in teaching, humanities, translations, as well as a long history of people-to-people work.</p>
+        <p>Hi friend! I'm Ana, a tech writer and software developer.</p>
+          
+        <p>In my latest full-time role, I was a Software Engineer at Zendesk, where I built API integrations and iPaaS connectors for Zendesk Action Builder, as well as wrote thorough docs and guides for what my team and I built.</p>
 
-          <p>My favourite things in the whole world are clarity, knowledge-sharing and making sense of complex things. I seek those in everything I do, be it doc-writing, coding or interacting with fellow human beings.</p>
+        <p>I also contribute to open-source academic software: currently I'm developing docs for <a href='https://github.com/Harvard-Neutrino/prometheus'>Prometheus</a>, a particle detector simulator developed by a global collaboration of physicists at Harvard and the University of Iowa.</p>
+        
+        <p>My favourite things in the whole world are clarity, structure and making sense of complex things. I seek those in everything I do, be it doc-writing, coding or interacting with fellow human beings.</p>
 
-          <p>I'm also a big company culture and belonging advocate. In my latest full time role I gave back to the community by volunteering as a representative on the ANZ site council. I really enjoy creating nurturing environments wherever I find myself.</p>
+        <p>In my free time I like to write music, paint boardgame miniatures and hang out with my cat Borsch. When the weather is nice, I like to get out of town for a hike.</p>
 
-          <p>In my free time I like to write music, paint boardgame miniatures or hang out with my cat Borsch. When the weather is nice, I like to get out of town for a hike.</p>
-
-          <p>If you would like to connect, work together or just say hi, feel free to get in touch with me <a href='https://www.linkedin.com/in/ana-lastoviria/' target='_blank'>on LinkedIn</a>, I would love to hear from you.</p>
+        <p>If you would like to connect, work together or just say hi, feel free to get in touch with me <a href='https://www.linkedin.com/in/ana-lastoviria/' target='_blank'>on LinkedIn</a>, I would love to hear from you.</p>
       </div>
     </div>
   );
